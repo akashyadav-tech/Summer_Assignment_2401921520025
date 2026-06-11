89d5eac397a297f0774d3937137b9daf6a47fcb8
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-06-11 20:38:57 +0530

    Create reverse-words-in-a-string-iii.cpp

---
+++ Week_2/Day_4/reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string temp="";
+        string ans="";
+
+        for(int i=0; i<s.length();i++){
+            if(s[i]!=' '){
+                temp+=s[i];
+            }
+            else{
+            reverse(temp.begin(),temp.end());
+            ans+=temp+' ';
+            temp="";
+            }
+        }
+       reverse(temp.begin(), temp.end());
+        ans += temp;
+        return ans;
+    }
+};
