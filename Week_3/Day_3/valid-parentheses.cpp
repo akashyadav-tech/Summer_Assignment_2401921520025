502742002be6971e1ec7514dc89def7774c0fc25
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-06-17 18:10:31 +0530

    Create valid-parentheses.cpp

---
+++ Week_3/Day_3/valid-parentheses.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+
+        for(int i=0; i<s.length();i++){
+            char ch=s[i];
+            if( ch =='(' || ch == '{' || ch == '['){
+                stk.push(ch);
+            }
+
+            if(!stk.empty()){
+                if( ch ==')' ){
+                if(stk.top()=='('){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+
+            }
+
+            if( ch =='}' ){
+                if(stk.top()=='{'){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            if( ch ==']' ){
+                if(stk.top()=='['){
+                    stk.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+            }
+            else{
+                return false;
+            }        
+        }
+
+        if(stk.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
