f64c347088e833a9b9c85c4e83a929fa693d0725
akashyadav-tech <akashyadav.12.a.5953@gmail.com>
2026-06-29 07:46:01 +0530

    Update Ques_2_OuterDemo.java

--- Week_4/OOPS/Ques_2_OuterDemo.java
+++ Week_4/OOPS/Ques_2_OuterDemo.java
@@ -1,1 +1,25 @@
+class Outer {
 
+    void display() {
+        System.out.println("This is Outer Class.");
+    }
+
+    class Inner {
+
+        void display() {
+            System.out.println("This is Inner Class.");
+        }
+    }
+}
+
+public class OuterDemo {
+
+    public static void main(String[] args) {
+
+        Outer obj = new Outer();
+        obj.display();
+
+        Outer.Inner obj1 = obj.new Inner();
+        obj1.display();
+    }
+}
